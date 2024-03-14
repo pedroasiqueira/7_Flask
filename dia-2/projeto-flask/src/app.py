84dbdca5f3702cf3381e50e759476c07c5995c3e
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-03-14 15:37:10 -0300

    Projeto-flask

---
+++ dia-2/projeto-flask/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import random
+from os import environ
+from waitress import serve
+
+app = Flask(__name__)
+
+joke_list = [
+    "Por que o bombeiro não gosta de andar? <br> Porque ele socorre.",
+    "Sabe como chama a sorveteria do Michel Teló? <br> Ice te Pego.",
+    "Por que o espanador não luta caratê? <br> Porque ele luta capoeira",
+]
+
+
+@app.route("/api/joke")
+def joke():
+    return jsonify({"joke": random.choice(joke_list)})
+
+
+def start_server(host: str = "0.0.0.0", port: int = 8000):
+    if environ.get("FLASK_ENV") == "dev":
+        # Servidor de desenvolvimento do Kit Werkzeug
+        return app.run(debug=True, host=host, port=port)
+    else:
+        # Este é o waitress, otimizado para produção
+        serve(app, host=host, port=port)
+
+
+if __name__ == "__main__":
+    start_server()
