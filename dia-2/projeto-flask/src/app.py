20955498ca9fba192a61d16988cc9213398670ef
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-03-17 11:38:59 -0300

    Final do conteudo do dia 3.2

--- dia-2/projeto-flask/src/app.py
+++ dia-2/projeto-flask/src/app.py
@@ -1,20 +1,10 @@
-from flask import Flask, jsonify
-import random
+from flask import Flask
+from controllers.jokes_controller import jokes_controller
 from os import environ
 from waitress import serve
 
 app = Flask(__name__)
-
-joke_list = [
-    "Por que o bombeiro não gosta de andar? <br> Porque ele socorre.",
-    "Sabe como chama a sorveteria do Michel Teló? <br> Ice te Pego.",
-    "Por que o espanador não luta caratê? <br> Porque ele luta capoeira",
-]
-
-
-@app.route("/api/joke")
-def joke():
-    return jsonify({"joke": random.choice(joke_list)})
+app.register_blueprint(jokes_controller, url_prefix="/jokes")
 
 
 def start_server(host: str = "0.0.0.0", port: int = 8000):
