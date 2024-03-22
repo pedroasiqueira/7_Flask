609e7e24308e028fd2d9e69c3b6d0bcf89c97aa4
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-03-22 19:54:46 -0300

    algo

---
+++ dia-2/projeto-flask/src/controllers/musics_controller.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify, request
+from models.music_model import MusicModel
+
+musics_controller = Blueprint("music", __name__)
+
+
+@musics_controller.route("/", methods=["POST"])
+def music_post():
+    new_music = MusicModel(request.json)
+    new_music.save()
+    return jsonify(new_music.to_dict()), 201
